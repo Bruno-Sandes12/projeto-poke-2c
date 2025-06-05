4a926df46d4b9d8e64d0e6437cf99fcdbed1b75a
Bruno Sandes <rm94640@estudante.fieb.edu.br>
2025-06-05 11:03:02 +0000

    Menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,11 +1,21 @@
-const Menu = () = > {
+const Menu = () => {
 
-    return(
+    return (
         <div className= "Menu">
             <ul className="nav">
                 <li className="nav-item">
-                <a className="nav-link active" href= "/home">
-                    Home
+                    <a className="nav-link active" href= "/home">
+                       Home
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" href= "/pokeflex">
+                       PokeFlex
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" href= "/pokegrid">
+                       Pokegrid
                     </a>
                 </li>
             </ul>
